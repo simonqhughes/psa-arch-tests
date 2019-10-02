acad60c96976aff5a201db2aec6b2c1033234506
Simon Hughes <simon.hughes@arm.com>
2019-10-02 10:38:56 +0100

    Fix PSA Storage test 406 - flags not supported test.
    The following provides more information on this commit:
    - The above test implements the setting and getting of
      object creation flags without regard for the behaviour
      of the flags. This fix skips set()/get_info() tests
      for the affested flags.
    - The test should be fixed to better reflect the
      behaviour of the flags.

--- api-tests/dev_apis/internal_trusted_storage/test_s006/test_s006.c
+++ api-tests/dev_apis/internal_trusted_storage/test_s006/test_s006.c
@@ -44,6 +44,7 @@ static int32_t psa_sst_remove_api(psa_storage_uid_t uid, uint32_t data_len,
     /* Call the get_info function and match the attributes */
     status = SST_FUNCTION(s006_data[2].api, uid, &info);
     TEST_ASSERT_EQUAL(status, s006_data[2].status, TEST_CHECKPOINT_NUM(2));
+    printf("create_flag=0x%x, info.flags=0x%x\n", create_flag, info.flags);
     TEST_ASSERT_EQUAL(info.flags, create_flag, TEST_CHECKPOINT_NUM(3));
 
     /* Remove the UID  */
@@ -63,8 +64,58 @@ int32_t psa_sst_flags_not_supported(caller_security_t caller)
    /* Calling set function with different create flag value */
 
    val->print(PRINT_TEST, "[Check 1] Call set API with valid flag values\n", 0);
+
+   /* The test is incorrect in that it sets a flag bit when an object
+    * is created, and expects the bit always to be set in the flags retrieved by
+    * psa_xx_get_info(). The specification defines this is not always the case.
+    *
+    * The following outlines psuedo-code for this test:
+    *
+    *  set create_flags = 0x800000000
+    *  while (create_flags != 0)
+    *    {
+    *      psa_ps_set()
+    *      psa_ps_get()
+    *      // check xxx
+    *      psa_ps_get_info()
+    *      // check flag retrieved is the same at the value used to set
+    *      psa_ps_remove()
+    *      create_flags >>= 1
+    *    }
+    * However:
+    *   - This test doesn't respect the behaviour of defined flags:
+    *       - PSA_STORAGE_FLAG_WRITE_ONCE
+    *           - If this bit is set then the object cannot be deleted.
+    *             psa_xx_remove() will fail but the test current checks the
+    *             object can successfully be removed.
+    *       - PSA_STORAGE_FLAG_NO_CONFIDENTIALITY
+    *           - The psa_trusted_storage_linux implementation always stores
+    *             files with authentication (because files are stored with
+    *             ecryptfs). Therefore if _NO_CONFIDENTIALITY is requested then
+    *             its accepted but files are still stored confidentially. This
+    *             is allowed by the spec. The spec also says that if this
+    *             happens the the _NO_CONFIDENTIALITY flag should not be
+    *             reported with get_info().
+    *       - PSA_STORAGE_FLAG_NO_REPLAY_PROTECTION
+    *           - This flags should be observed with the psa_xx_get_info()
+    *             after creation.
+    *  - Only 3 of the flag bits are defined. The specification should be
+    *    modified to say something 1) the undefined flag bits
+    *    are reserved and the operation of the flags is undefined, or 2)
+    *    if flag bit is set then the value should be retained with the object
+    *    i.e. the behaviour being tested for here.
+    * The solution if to not test for the following flags:
+    *   - PSA_STORAGE_FLAG_WRITE_ONCE
+    *   - PSA_STORAGE_FLAG_NO_CONFIDENTIALITY
+    */
+
    while (flag)
    {
+       if ( (flag & PSA_STORAGE_FLAG_WRITE_ONCE) || (flag & PSA_STORAGE_FLAG_NO_CONFIDENTIALITY) )
+       {
+           flag = flag >> 1;
+           continue;
+       }
        /* Create storage with flag value */
        status = SST_FUNCTION(s006_data[1].api, uid, TEST_BUFF_SIZE, write_buff,
                                                          (flag & (~PSA_STORAGE_FLAG_WRITE_ONCE)));
